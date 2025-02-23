8935eb26f3baece32593ef239d30ef6b5fbf309e
Hippolyte Verninas <hverninas-22@gpu3.enst.fr>
2025-02-23 18:54:29 +0100

    feat: new training using transformers

--- preprocessing.py
+++ preprocessing.py
@@ -1,11 +1,67 @@
 import pandas as pd
 from dont_patronize_me import DontPatronizeMe
 from typing import Tuple
+from torch.utils.data import Dataset, DataLoader
+import torch
+import re
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+
+
+
+class DPMDataset(Dataset):
+    """
+    Dataset for regular use with Roberta/Deberta
+    
+    """
+    def __init__(self, df, tokenizer, max_len, test_set=False):
+        
+        self.test_set = test_set
+        self.tokenizer = tokenizer
+        self.text = df.text
+
+        if not test_set:
+            self.label = df.label
+
+        self.max_len = max_len
+
+    def __len__(self):
+        return len(self.text)
+
+    def __getitem__(self, idx):
+        if not self.test_set:
+            return {
+                'text': self.text[idx],
+                'target': self.label[idx]
+            }
+        else:
+            return {
+                'text': self.text[idx],
+            }
+
+
+    def collate_fn(self, batch):
+        texts = []
+        labels = []
+
+
+        for b in batch:
+            texts.append(b['text'])
+            if not self.test_set:
+                labels.append(b['target'])
+
+        encodings = self.tokenizer(texts, return_tensors='pt', padding=True, truncation=True, max_length=self.max_len)
+        if not self.test_set:
+            encodings['target'] = torch.tensor(labels)
+
+        return encodings
+
 
 def labels2file(p, outf_path):
     with open(outf_path,'w') as outf:
         for pi in p:
             outf.write(','.join([str(k) for k in pi])+'\n')
+
 
 def load_data() -> Tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame]:
     """
@@ -64,13 +120,57 @@ def load_data() -> Tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame]:
 
     return trdf1, tedf1, data_test
 
+
+def clean_text(text: str, whitespacing = True, standard_tokens = True, punctuation=True, stop_words = False,lower=False) -> str:
+
+        stop_words = stopwords.words("english")
+        
+        if stop_words:
+            # Stop words --> token
+            words = word_tokenize(text)
+            words = [word for word in words if word not in stopwords.words("english") and word.isalpha()]
+            text = " ".join(words)
+
+        if lower:
+             text=text.lower()
+
+        if whitespacing:
+            # Whtespacing and other standarisation
+            text = text.strip('"')  # removing " at start of sentences
+            text = re.sub(r'\s+', ' ', text)
+            text = re.sub('<h>', '.', text)
+
+        if standard_tokens:
+            # Removing unecessary info
+            text = re.sub("(?<![\w])[0-9]+[.,]?[0-9]*(?![\w])", "[NUM]", text)
+            text = re.sub("\[NUM\]-\[NUM\]", "[NUM]", text)
+            # To delete account numbers 12-5223-231
+            text = re.sub("\[NUM\]-\[NUM\]", "[NUM]", text)
+            text = re.sub(r"https? : \S+", "[WEBSITE]", text)  # Tokenize links
+            text = re.sub("(?<![\w])20[0-5][0-9]-?[0-9]*", "[YEAR]", text)  # Year token
+            text = re.sub(r"@[\S]+", "[USERNAME]", text)  # removing referencing on usernames with @
+            text = re.sub("(?<![\w])1[0-9]{3}-?[0-9]*", "[YEAR]", text)  # Year token
+            #text = re.sub("(?<=\[NUM\])-(?=[a-zA-Z])", " ", text)
+
+        if punctuation:
+            text = re.sub(r":\S+", "", text)  # removing smileys with : (like :),:D,:( etc)
+            text = re.sub(r"\"+", "", text)  # replacing repetitions of punctations
+            text = re.sub(r"(\W)(?=\1)", "", text)  # replacing repetitions of punctations
+            
+        return text.strip()
+
+
 def preprocess_data(df: pd.DataFrame) -> pd.DataFrame:
     """
     Preprocess the data to be used by the model
     """
     df = df.copy()
-    df["text"] = df.apply(lambda row: f"This concerns the {row['community']}. {row['text']}", axis=1)
+    # df["text"] = df.apply(lambda row: f"This concerns the {row['community']}. {row['text']}", axis=1)
+    df["text"] = df["text"].apply(lambda x: clean_text(x))
     return df
+
+
+
 
 
 if __name__ == "__main__":
